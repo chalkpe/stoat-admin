6a50eac4273bb0ef39c096cded6f1ff85224e770
Chalk <9481405+chalkpe@users.noreply.github.com>
2025-11-11 02:40:02 +0900

    feat: x86 node based image

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,20 @@
-FROM node:24
+FROM --platform=linux/amd64 node:24
 WORKDIR /app
 
+RUN apt-get update && apt-get install -y \
+    python3 \
+    make \
+    g++ \
+    libstdc++6 \
+    libssl3 \
+    zlib1g \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY package*.json ./
+RUN npm ci
+
 COPY . .
-RUN npm install --frozen-lockfile && npm run build
+RUN npm run build
 
 EXPOSE 3000/tcp
 ENTRYPOINT [ "npm", "run", "start" ]
--- next.config.js
+++ next.config.js
@@ -1,24 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ['revolt-nodejs-bindings'],
   experimental: {
     reactCompiler: true,
   },
-  webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
-    config.watchOptions = {
-      ignored: ['**/*'],
-    };
-    config.module.rules.push({
-      test: /\.node$/,
-      use: [
-        {
-          loader: "nextjs-node-loader",
-          options: {
-            // flags: os.constants.dlopen.RTLD_NOW,
-            outputPath: config.output.path,
-          },
-        },
-      ],
-    });
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'revolt-nodejs-bindings': 'commonjs revolt-nodejs-bindings',
+      });
+    }
     return config;
   },
 };
